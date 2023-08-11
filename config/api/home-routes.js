98bcdde579bab60032ff4e4975b1ec6b3a06961f
amtune <allisontune@gmail.com>
2023-08-11 16:05:39 -0400

    homeroutes4

--- config/api/home-routes.js
+++ config/api/home-routes.js
@@ -44,4 +44,55 @@ router.get("/", async (req, res) => {
     res.render("login");
   });
 
+  router.get("/signup", (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect("/");
+      return;
+    }
+  
+    res.render("signup");
+  });
+
+  router.get("/post/:id", async (req, res) => {
+    try {
+      const dbPostData = await Post.findOne({
+        where: {
+          id: req.params.id,
+        },
+        attributes: ["id", "content", "title", "created_at"],
+        include: [
+          {
+            model: Comment,
+            attributes: [
+              "id",
+              "comment_text",
+              "post_id",
+              "user_id",
+              "created_at",
+            ],
+            include: {
+              model: User,
+              attributes: ["username"],
+            },
+          },
+          {
+            model: User,
+            attributes: ["username"],
+          },
+        ],
+      });
+  
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      const post = dbPostData.get({ plain: true });
+      console.log(post);
+      res.render("single-post", { post, loggedIn: req.session.loggedIn });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+
   
