916049b896cc6c8a54b088f000566b0744f7f2b6
amtune <allisontune@gmail.com>
2023-08-11 16:04:59 -0400

    homeroutes3

--- config/api/home-routes.js
+++ config/api/home-routes.js
@@ -28,4 +28,20 @@ router.get("/", async (req, res) => {
         ],
       });
 
-      
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render("homepage", { posts, loggedIn: req.session.loggedIn });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+
+  router.get("/login", (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect("/");
+      return;
+    }
+    res.render("login");
+  });
+
+  
