6940167f615e3626300244a5f96f71656ee5ad8b
amtune <allisontune@gmail.com>
2023-08-11 16:04:15 -0400

    homeroutes2

--- config/api/home-routes.js
+++ config/api/home-routes.js
@@ -2,3 +2,30 @@ const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 const router = require("express").Router();
 
+router.get("/", async (req, res) => {
+    try {
+      const dbPostData = await Post.findAll({
+        attributes: ["id", "title", "content", "created_at"],
+        include: [
+          {
+            model: Comment,
+            attributes: [
+              "id",
+              "comment_text",
+              "post_id",
+              "user_id",
+              "created_at",
+            ],
+            include: {
+              model: User,
+              attributes: ["username"],
+            },
+          },
+          {
+            model: User,
+            attributes: ["username"],
+          },
+        ],
+      });
+
+      
